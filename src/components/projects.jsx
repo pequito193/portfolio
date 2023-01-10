f4b1b2f394f6bc6e4384024f35cec75d71f765d0
pequito193 <vitorspequito@gmail.com>
2023-01-10 14:41:50 +0000

    Update projects component

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,10 +1,50 @@
 import React from "react";
+import './../styles/projects.css'
 
-function Projects() {
+function Projects(props) {
+
+    const { theme } = props;
 
     return(
         <React.Fragment>
-
+            <div className="project-wrapper">
+                <a href="https://pequito193.github.io/weather-app" target='_blank' rel='noreferrer'>
+                    <div className="container">
+                        <img src={require('./../assets/weather-app.png')} alt="preview of weather app project" className="project-image" />
+                        <div className="opaque" />
+                        <div className="project-name">Weather App</div>
+                    </div>
+                </a>
+                <a href="https://pequito193.github.io/memory-card" target='_blank' rel='noreferrer'>
+                    <div className="container">
+                        <img src={require('./../assets/memory-card.png')} alt="preview of memory card project" className="project-image" />
+                        <div className="opaque" />
+                        <div className="project-name">Memory Card</div>
+                    </div>
+                </a>
+                <a href="https://pequito193.github.io/shopping-cart" target='_blank' rel='noreferrer'>
+                    <div className="container">
+                        <img src={require('./../assets/shopping-cart.png')} alt="preview of shopping cart project" className="project-image" />
+                        <div className="opaque" />
+                        <div className="project-name">Shopping Cart</div>
+                    </div>
+                </a>
+                <div className="container">
+                    <img src="" alt="" className="project-image" />
+                    <div className="opaque" />
+                    <div className="project-name"></div>
+                </div>
+                <div className="container">
+                    <img src="" alt="" className="project-image" />
+                    <div className="opaque" />
+                    <div className="project-name"></div>
+                </div>
+                <div className="container">
+                    <img src="" alt="" className="project-image" />
+                    <div className="opaque" />
+                    <div className="project-name"></div>
+                </div>
+            </div>
         </React.Fragment>
     )
 }
