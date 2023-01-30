8fb8396187646fc06600a67d78d767e880150ca3
pequito193 <vitorspequito@gmail.com>
2023-01-30 18:07:58 +0000

    Update projects

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -31,21 +31,11 @@ function Projects(props) {
                 </a>
                 <a href="https://github.com/pequito193/forum-frontend">
                     <div className="container">
-                        <img src="" alt="preview of forum project" className="project-image" />
+                        <img src={require('./../assets/forum-frontend.png')} alt="preview of forum project" className="project-image" />
                         <div className="opaque" id={theme} />
                         <div className="project-name" id={theme}>Forum</div>
                     </div>
                 </a>
-                <div className="https://github.com/pequito193/inventory-application">
-                    <img src="" alt="preview of inventory catalog project" className="project-image" />
-                    <div className="opaque" id={theme} />
-                    <div className="project-name" id={theme}>Inventory Catalog</div>
-                </div>
-                <div className="container">
-                    <img src="" alt="" className="project-image" />
-                    <div className="opaque" id={theme} />
-                    <div className="project-name" id={theme}></div>
-                </div>
             </div>
         </React.Fragment>
     )
