6864f6a8689f9aba2f3a832950234f923b3f4e59
pequito193 <vitorspequito@gmail.com>
2023-01-30 17:47:02 +0000

    Update links

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -8,33 +8,35 @@ function Projects(props) {
     return(
         <React.Fragment>
             <div className="project-wrapper">
-                <a href="https://pequito193.github.io/weather-app" target='_blank' rel='noreferrer'>
+                <a href="https://github.com/pequito193/weather-app" target='_blank' rel='noreferrer'>
                     <div className="container">
                         <img src={require('./../assets/weather-app.png')} alt="preview of weather app project" className="project-image" />
                         <div className="opaque" id={theme} />
                         <div className="project-name" id={theme}>Weather App</div>
                     </div>
                 </a>
-                <a href="https://pequito193.github.io/memory-card" target='_blank' rel='noreferrer'>
+                <a href="https://github.com/pequito193/memory-card" target='_blank' rel='noreferrer'>
                     <div className="container">
                         <img src={require('./../assets/memory-card.png')} alt="preview of memory card project" className="project-image" />
                         <div className="opaque" id={theme} />
                         <div className="project-name" id={theme}>Memory Card</div>
                     </div>
                 </a>
-                <a href="https://pequito193.github.io/shopping-cart" target='_blank' rel='noreferrer'>
+                <a href="https://github.com/pequito193/shopping-cart" target='_blank' rel='noreferrer'>
                     <div className="container">
                         <img src={require('./../assets/shopping-cart.png')} alt="preview of shopping cart project" className="project-image" />
                         <div className="opaque" id={theme} />
                         <div className="project-name" id={theme}>Shopping Cart</div>
                     </div>
                 </a>
-                <div className="container">
-                    <img src="" alt="preview of forum project" className="project-image" />
-                    <div className="opaque" id={theme} />
-                    <div className="project-name" id={theme}>Forum</div>
-                </div>
-                <div className="container">
+                <a href="https://github.com/pequito193/forum-frontend">
+                    <div className="container">
+                        <img src="" alt="preview of forum project" className="project-image" />
+                        <div className="opaque" id={theme} />
+                        <div className="project-name" id={theme}>Forum</div>
+                    </div>
+                </a>
+                <div className="https://github.com/pequito193/inventory-application">
                     <img src="" alt="preview of inventory catalog project" className="project-image" />
                     <div className="opaque" id={theme} />
                     <div className="project-name" id={theme}>Inventory Catalog</div>
